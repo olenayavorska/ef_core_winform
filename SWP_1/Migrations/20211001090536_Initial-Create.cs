8563b05ca6bd6a329427579e1f44877f9de3d7ae
olenayavorska <olenayavorska46@gmail.com>
2021-10-01 12:09:38 +0300

    Add project files.

---
+++ SWP_1/Migrations/20211001090536_Initial-Create.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SWP_1.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    IdBook = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.IdBook);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sages",
+                columns: table => new
+                {
+                    IdSage = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    age = table.Column<int>(type: "int", nullable: false),
+                    photo = table.Column<int>(type: "int", nullable: false),
+                    city = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sages", x => x.IdSage);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SageBooks",
+                columns: table => new
+                {
+                    idSage = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SageIdSage = table.Column<int>(type: "int", nullable: true),
+                    IdBook = table.Column<int>(type: "int", nullable: false),
+                    BookIdBook = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SageBooks", x => x.idSage);
+                    table.ForeignKey(
+                        name: "FK_SageBooks_Books_BookIdBook",
+                        column: x => x.BookIdBook,
+                        principalTable: "Books",
+                        principalColumn: "IdBook",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SageBooks_Sages_SageIdSage",
+                        column: x => x.SageIdSage,
+                        principalTable: "Sages",
+                        principalColumn: "IdSage",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SageBooks_BookIdBook",
+                table: "SageBooks",
+                column: "BookIdBook");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SageBooks_SageIdSage",
+                table: "SageBooks",
+                column: "SageIdSage");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SageBooks");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Sages");
+        }
+    }
+}
---
+++ SWP_1/Program.cs
@@ -0,0 +1,76 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SWP_1
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    class Sage
+    {
+        //Sage(IdSage, name, age, photo, city)
+        [Key]
+        public int IdSage { get; set; }
+        public String name { get; set; }
+        public int age { get; set; }
+        public int photo { get; set; }
+        public String city { get; set; }
+        public List<SageBook> SageBooks { get; set; }
+
+
+    }
+    class Book
+    {
+        //Book(IdBook, name, description)
+        [Key]
+        public int IdBook { get; set; }
+        public String name { get; set; }
+        public String description { get; set; }
+        public List<SageBook> SageBooks { get; set; }
+
+    }
+    class SageBook
+    {
+        //SageBook(idSage, idBook).
+        [Key]
+        public int idSage { get; set; }
+        public Sage Sage { get; set; }
+        public int IdBook { get; set; }
+        public Book Book { get; set; }
+
+
+
+    }
+    class SageBookContext : DbContext
+    {
+        ///public DbSet<Sage> Sages 
+        public DbSet<Sage> Sages { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<SageBook> SageBooks { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=l1db;Trusted_Connection=True;");
+
+        }
+    }
+}
+
